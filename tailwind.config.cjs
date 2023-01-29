274873da21b90df83c41995554c6f1bfb0d38126
Gianfranco Palumbo <gianpa@gmail.com>
2023-01-29 13:32:12 +0100

    Fix accent / hover text color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,14 +23,14 @@ module.exports = {
     textColor: {
       skin: {
         base: withOpacity("--color-text-base"),
-        accent: withOpacity("--color-puerto-rico-600"),
+        accent: withOpacity("--accent"),
         inverted: withOpacity("--color-fill"),
       },
     },
     backgroundColor: {
       skin: {
         fill: withOpacity("--color-fill"),
-        accent: withOpacity("--color-puerto-rico-600"),
+        accent: withOpacity("--accent"),
         inverted: withOpacity("--color-text-base"),
         card: withOpacity("--color-card"),
         "card-muted": withOpacity("--color-card-muted"),
@@ -38,41 +38,26 @@ module.exports = {
     },
     outlineColor: {
       skin: {
-        fill: withOpacity("--color-puerto-rico-600"),
+        fill: withOpacity("--accent"),
       },
     },
     borderColor: {
       skin: {
         line: withOpacity("--color-border"),
         fill: withOpacity("--color-text-base"),
-        accent: withOpacity("--color-puerto-rico-600"),
+        accent: withOpacity("--accent"),
       },
     },
     fill: {
       skin: {
         base: withOpacity("--color-text-base"),
-        accent: withOpacity("--color-puerto-rico-600"),
+        accent: withOpacity("--accent"),
       },
       transparent: "transparent",
     },
     fontFamily: {
       mono: ["IBM Plex Mono", "monospace"],
     },
-    colors: {
-      "puerto-rico": {
-        50: "#f1fcf8",
-        100: "#d1f6eb",
-        200: "#a2edd7",
-        300: "#6cdcc1",
-        400: "#42c5a9",
-        500: "#24a88d",
-        600: "#1a8773",
-        700: "#196c5e",
-        800: "#19564d",
-        900: "#194841",
-      },
-    },
-    // },
   },
   plugins: [require("@tailwindcss/typography")],
 };
