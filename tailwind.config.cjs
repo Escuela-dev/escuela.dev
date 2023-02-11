ae4fa7634fd37aa049d6706a290d38d8d4048f72
Gianfranco Palumbo <gianpa@gmail.com>
2023-02-11 20:35:41 +0100

    fix ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,55 +12,117 @@ module.exports = {
     "./src/**/*.{astro,html,md,mdx,ts,tsx}",
     "./node_modules/flowbite/**/*.js",
   ],
+  darkMode: "class", // or 'class'
   theme: {
     // Remove the following screen breakpoint or add other breakpoints
     // if one breakpoint is not enough for you
     screens: {
       sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
+    colors: ({ colors }) => ({
+      ...colors,
+      primary: "#9333EA",
+      secondary: "#ff7e33",
+      info: "#0C63E7",
+      gray: {
+        50: "#FAFAFC",
+        100: "#E9E9EC",
+        200: "#C6C8CD",
+        300: "#ACAEB6",
+        400: "#92959F",
+        500: "#777C87",
+        600: "#5D6370",
+        700: "#434959",
+        800: "#293041",
+        900: "#0f172a",
+      },
+    }),
 
-    // Uncomment the following extend
-    // if existing Tailwind color palette will be used
-
-    // extend: {
-    textColor: {
-      skin: {
-        base: withOpacity("--color-text-base"),
-        accent: withOpacity("--accent"),
-        inverted: withOpacity("--color-fill"),
+    extend: {
+      textColor: {
+        skin: {
+          base: withOpacity("--color-text-base"),
+          accent: withOpacity("--accent"),
+          inverted: withOpacity("--color-fill"),
+        },
       },
-    },
-    backgroundColor: {
-      skin: {
-        fill: withOpacity("--color-fill"),
-        accent: withOpacity("--accent"),
-        inverted: withOpacity("--color-text-base"),
-        card: withOpacity("--color-card"),
-        "card-muted": withOpacity("--color-card-muted"),
+      backgroundColor: {
+        skin: {
+          fill: withOpacity("--color-fill"),
+          accent: withOpacity("--accent"),
+          inverted: withOpacity("--color-text-base"),
+          card: withOpacity("--color-card"),
+          "card-muted": withOpacity("--color-card-muted"),
+        },
       },
-    },
-    outlineColor: {
-      skin: {
-        fill: withOpacity("--accent"),
+      outlineColor: {
+        skin: {
+          fill: withOpacity("--accent"),
+        },
       },
-    },
-    borderColor: {
-      skin: {
-        line: withOpacity("--color-border"),
-        fill: withOpacity("--color-text-base"),
-        accent: withOpacity("--accent"),
+      borderColor: {
+        skin: {
+          line: withOpacity("--color-border"),
+          fill: withOpacity("--color-text-base"),
+          accent: withOpacity("--accent"),
+        },
       },
-    },
-    fill: {
-      skin: {
-        base: withOpacity("--color-text-base"),
-        accent: withOpacity("--accent"),
+      fill: {
+        skin: {
+          base: withOpacity("--color-text-base"),
+          accent: withOpacity("--accent"),
+        },
+        transparent: "transparent",
       },
-      transparent: "transparent",
-    },
-    fontFamily: {
-      mono: ["IBM Plex Mono", "monospace"],
+      fontFamily: {
+        mono: ["IBM Plex Mono", "monospace"],
+      },
+      blur: {
+        0: "0",
+        none: "0",
+        sm: "4px",
+        DEFAULT: "8px",
+        md: "12px",
+        lg: "16px",
+        xl: "24px",
+        "2xl": "40px",
+        "3xl": "64px",
+      },
+      brightness: {
+        0: "0",
+        50: ".5",
+        75: ".75",
+        90: ".9",
+        95: ".95",
+        100: "1",
+        105: "1.05",
+        110: "1.1",
+        125: "1.25",
+        150: "1.5",
+        200: "2",
+      },
+      backdropBlur: ({ theme }) => theme("blur"),
+      gradientColorStops: ({ theme }) => theme("colors"),
+      backgroundImage: {
+        none: "none",
+        "gradient-to-t": "linear-gradient(to top, var(--tw-gradient-stops))",
+        "gradient-to-tr":
+          "linear-gradient(to top right, var(--tw-gradient-stops))",
+        "gradient-to-r": "linear-gradient(to right, var(--tw-gradient-stops))",
+        "gradient-to-br":
+          "linear-gradient(to bottom right, var(--tw-gradient-stops))",
+        "gradient-to-b": "linear-gradient(to bottom, var(--tw-gradient-stops))",
+        "gradient-to-bl":
+          "linear-gradient(to bottom left, var(--tw-gradient-stops))",
+        "gradient-to-l": "linear-gradient(to left, var(--tw-gradient-stops))",
+        "gradient-to-tl":
+          "linear-gradient(to top left, var(--tw-gradient-stops))",
+      },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")],
 };
