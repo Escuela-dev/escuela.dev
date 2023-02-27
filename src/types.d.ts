706410102d719d00732de822bb4b74508e97a4df
Gianfranco Palumbo <gianpa@gmail.com>
2023-02-27 20:11:00 +0100

    readingTime and article:published_time

---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+declare interface HeadProps {
+  title?: string;
+  author?: string;
+  description?: string;
+  ogImage?: string;
+  type?: "website" | "article";
+  publishDate?: string;
+}
---
+++ src/utils/getReadingTime.ts
@@ -0,0 +1,11 @@
+
+const WORDS_PER_MINUTE = 200;
+
+const getReadingTime = (content?: string) => {
+  if (!content) return;
+  const clean = content.replace(/<\/?[^>]+(>|$)/g, '');
+  const numberOfWords = clean.split(/\s/g).length;
+  return Math.ceil(numberOfWords / WORDS_PER_MINUTE);
+}
+
+export default getReadingTime;
