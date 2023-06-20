35418a6fd2600c3698f1ba45b840e340f2cacac6
Gianfranco Palumbo <gianpa@gmail.com>
2023-06-20 11:10:18 +0200

    dynamic month and year

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,24 @@
+const nextMonth = new Date(
+  new Date().getFullYear(),
+  new Date().getMonth() + 1,
+  1
+);
+const nextMonthName = nextMonth.toLocaleDateString("en-GB", { month: "long" });
+
 export const SITE = {
-    // websiteUrl: "https://escuela.dev/",
-    author: "Gianfranco Palumbo",
-    desc: "A Coding school in Malaga, Spain - Enroll to the next batch in April 2023 - Escuela de Programación y Desarrollo Web",
-    seoTitle: "𝙴𝚜𝚌𝚞𝚎𝚕𝚊.𝚍𝚎𝚟 - Coding school",
-    title: "Escuela.dev - Coding school",
-    ogImage: "escuela.dev-coding-school.png",
-    lightAndDarkMode: true,
-    postPerPage: 3,
+  // websiteUrl: "https://escuela.dev/",
+  author: "Gianfranco Palumbo",
+  desc: `A Coding school in Malaga, Spain - Enroll to the next batch in ${nextMonthName} ${new Date().getFullYear()} - Escuela de Programación y Desarrollo Web`,
+  seoTitle: "𝙴𝚜𝚌𝚞𝚎𝚕𝚊.𝚍𝚎𝚟 - Coding school",
+  title: "Escuela.dev - Coding school",
+  ogImage: "escuela.dev-coding-school.png",
+  lightAndDarkMode: true,
+  postPerPage: 3,
 };
 
 export const LOGO_IMAGE = {
-    enable: false,
-    svg: true,
-    width: 216,
-    height: 46,
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
 };
