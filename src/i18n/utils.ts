462bc44bc68522a2eb0f8b6140be5e53c9e2b1c0
Gianfranco Palumbo <gianpa@gmail.com>
2024-11-22 15:40:09 +0100

    fix /free-class translation

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -18,7 +18,10 @@ export function useTranslations(lang: Language) {
     // @ts-expect-error - type error
                           keys.reduce((obj, key) => obj?.[key], strings[defaultLang] as Record<string, string>);
 
-    return translatedString || "";
+    if (!translatedString) {
+      throw new Error(`Missing translation for key ${key}`);
+    }
+    return translatedString
   };
 }
 
