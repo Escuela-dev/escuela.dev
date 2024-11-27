85d1ff32c9e8e0530eb04a5cdadc9d3414f62fb0
Gianfranco Palumbo <gianpa@gmail.com>
2024-11-27 11:31:23 +0100

    autoformatting

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -1,5 +1,48 @@
 import { reference, z } from 'astro:content';
 
+export const BlogNames = z.enum([
+  'JavaScript',
+  'TypeScript',
+  'Developer Tools',
+  'React',
+  'NodeJS',
+  'TailwindCSS',
+  'NextJS',
+  'Svelte',
+  'VScode',
+  'Astro',
+  'Others',
+]);
+export const BlogSlugs = z.enum([
+  'javascript',
+  'typescript',
+  'developer-tools',
+  'coding',
+  'react',
+  'development',
+  'nodejs',
+  'tailwindcss',
+  'nextjs',
+  'svelte',
+  'vscode',
+  'astro',
+  'youtube',
+  'tutorial',
+  'leetcode',
+  'interviews',
+  'career',
+  'salary',
+  'learning',
+  'negotiation',
+  'others',
+]);
+
+export const tagSchema = z
+  .object({
+    name: BlogNames,
+    slug: BlogSlugs,
+  })
+  .strict();
 
 export const blogSchema = z
   .object({
@@ -10,14 +53,17 @@ export const blogSchema = z
     featured: z.boolean().optional(),
     draft: z.boolean().optional(),
     tags: z.array(z.string()).max(5).default(["others"]),
+    // tags: z.array(tagSchema).max(5),
     heroImage: z.string().optional(),
     ogImage: z.string().optional(),
     imageCaption: z.string().optional(),
     imagePromptfile: z.string().optional(),
-    description: z.string()
+    description: z.string(),
     // Reference an array of related posts from the `blog` collection by `slug`
     // relatedPosts: z.array(reference('blog')),
   })
   .strict();
 
 export type BlogFrontmatter = z.infer<typeof blogSchema>;
+
+export type Tag = z.infer<typeof tagSchema>;
