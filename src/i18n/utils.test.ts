bde3c55a98f1def8d89006cfc524de49b65b1aa1
Gianfranco Palumbo <gianpa@gmail.com>
2024-12-04 15:11:51 +0100

    fix getRelativeLocaleUrl test

--- src/i18n/utils.test.ts
+++ src/i18n/utils.test.ts
@@ -2,28 +2,28 @@ import { describe, it, expect } from 'vitest';
 
 import { getRelativeLocaleUrl } from './utils';
 
-describe('useTranslatedPath', () => {
+describe('getRelativeLocaleUrl', () => {
   describe('es', () => {
     it('should return the path with the language prefix', () => {
-      expect(getRelativeLocaleUrl('es', '/')).toBe('/');
+      expect(getRelativeLocaleUrl('es', '/')).toBe('/es/');
     });
     it('should return the path with the language prefix', () => {
-      expect(getRelativeLocaleUrl('es', '/es')).toBe('/');
+      expect(getRelativeLocaleUrl('es', '/es')).toBe('/es/');
     });
     it('should return the path with the language prefix', () => {
-      expect(getRelativeLocaleUrl('es', '/es/')).toBe('/');
+      expect(getRelativeLocaleUrl('es', '/es/')).toBe('/es/');
     });
   });
   // showDefaultLang = false
   describe('en', () => {
     it('should return the path with the language prefix', () => {
-      expect(getRelativeLocaleUrl('en', '/')).toBe('/en');
+      expect(getRelativeLocaleUrl('en', '/')).toBe('/en/');
     });
     it('should return the path with the language prefix', () => {
-      expect(getRelativeLocaleUrl('en', '/en')).toBe('/en');
+      expect(getRelativeLocaleUrl('en', '/en')).toBe('/en/');
     });
     it('should return the path with the language prefix', () => {
-      expect(getRelativeLocaleUrl('en', '/en/')).toBe('/en');
+      expect(getRelativeLocaleUrl('en', '/en/')).toBe('/en/');
     });
   });
 });
